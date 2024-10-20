b90e766bef4c927145bcca4b783ffd7d26ed7730
gabrielcaser <gabrielcaser9@hotmail.com>
2024-10-20 17:34:57 -0400

    renv files

--- 00_main_code.R
+++ 00_main_code.R
@@ -9,12 +9,14 @@
 rm(list = ls(all.names = TRUE)) # clear objects
 gc() # free up memory
 
+
+# Downloads required libraries --------------------------------------------
+
+renv::restore()
+
 # Libraries ---------------------------------------------------------------
 library("tidyverse")
-
-
 library("estimatr")
-
 library("modelsummary")
 library('geobr')
 library('skimr') # to create sumstats
@@ -22,8 +24,10 @@ library('rdrobust')
 library('patchwork') # to create figures with plots together
 
 # Setting -----------------------------------------------------------------
-
-
 work_dir                       = "C:/Users/gabri/OneDrive/Gabriel/Insper/Tese/Engenheiros/replication_code/rdd_when_science_strikes_back/sum_stats"
 output_dir                     = "C:/Users/gabri/OneDrive/Gabriel/Insper/Tese/Engenheiros/replication_code/rdd_when_science_strikes_back/sum_stats/output"
 create_dataset_for_regressions = "C:/Users/gabri/OneDrive/Gabriel/Insper/Tese/Engenheiros/replication_code/rdd_when_science_strikes_back/6_create_rdd_dataset/output"
+
+# Running scripts ---------------------------------------------------------
+
+
